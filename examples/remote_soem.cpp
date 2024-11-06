fe6579c350945c54f634faa9120e0a2dab243ec5
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-11-06 09:33:38 +0900

    use undersocre instead of hyphen

--- examples/remote_soem.cpp
+++ examples/remote_soem.cpp
@@ -1,11 +1,15 @@
-#include "autd3-link-soem.hpp"
 #include "autd3.hpp"
+#include "autd3_link_soem.hpp"
 #include "util.hpp"
 
 int main() try {
-  auto autd = autd3::ControllerBuilder(std::vector{autd3::AUTD3(autd3::Vector3::Zero())}).open(autd3::link::RemoteSOEM::builder("127.0.0.1:8080"));
+  auto autd = autd3::ControllerBuilder(
+                  std::vector{autd3::AUTD3(autd3::Vector3::Zero())})
+                  .open(autd3::link::RemoteSOEM::builder("127.0.0.1:8080"));
 
-  autd.send((autd3::modulation::Sine(150 * autd3::Hz), autd3::gain::Focus(autd.center() + autd3::Vector3(0.0, 0.0, 150.0))));
+  autd.send(
+      (autd3::modulation::Sine(150 * autd3::Hz),
+       autd3::gain::Focus(autd.center() + autd3::Vector3(0.0, 0.0, 150.0))));
 
   std::string in;
   getline(std::cin, in);
