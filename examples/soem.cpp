a499d03d4acde6e6084233eefecdda7a05e9012f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-11-06 09:38:08 +0900

    format

--- examples/soem.cpp
+++ examples/soem.cpp
@@ -15,22 +15,17 @@ int main() try {
 
   autd3::tracing_init();
 
-  auto autd =
-      autd3::ControllerBuilder(
-          std::vector{autd3::AUTD3(autd3::Vector3::Zero())})
-          .open(autd3::link::SOEM::builder().with_err_handler(
-              [](const uint16_t slave, const autd3::link::Status status) {
-                std::cout << "slave [" << slave << "]: " << status << std::endl;
-                if (status == autd3::link::Status::Lost()) {
-                  // You can also wait for the link to recover, without exiting
-                  // the process
-                  exit(-1);
-                }
-              }));
+  auto autd = autd3::ControllerBuilder(std::vector{autd3::AUTD3(autd3::Vector3::Zero())})
+                  .open(autd3::link::SOEM::builder().with_err_handler([](const uint16_t slave, const autd3::link::Status status) {
+                    std::cout << "slave [" << slave << "]: " << status << std::endl;
+                    if (status == autd3::link::Status::Lost()) {
+                      // You can also wait for the link to recover, without exiting
+                      // the process
+                      exit(-1);
+                    }
+                  }));
 
-  autd.send(
-      (autd3::modulation::Sine(150 * autd3::Hz),
-       autd3::gain::Focus(autd.center() + autd3::Vector3(0.0, 0.0, 150.0))));
+  autd.send((autd3::modulation::Sine(150 * autd3::Hz), autd3::gain::Focus(autd.center() + autd3::Vector3(0.0, 0.0, 150.0))));
 
   std::string in;
   getline(std::cin, in);
